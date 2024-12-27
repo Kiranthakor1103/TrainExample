89d2103ef5f6cb6108f989602cc0792fde3c5e5d
THAKOR KIRANKUMAR RANJITSINH <thakorkiran1012003@gmail.com>
2024-12-27 15:17:35 +0530

    change into the file

--- train4.js
+++ train4.js
@@ -6,9 +6,10 @@ function calculateTrainRounds(list, maxCapacity) {
   let leftBehind = [];
 
   for (let i = 0; i < list.length; i++) {
+
     let group = list[i];
 
-    // While the group exceeds the max capacity, split it
+    // While the group size is greater than  max capacity, split it
     while (group > maxCapacity) {
       rounds.push([maxCapacity]); // Add a full round of max capacity
       emptySeats.push(0); // No empty seats in a full round
@@ -33,20 +34,20 @@ function calculateTrainRounds(list, maxCapacity) {
     }
   }
 
-  // Push the final round if there's any leftover people
+  // Push the final round if there's any leftover in current round people
   if (currentRound.length > 0) {
     rounds.push(currentRound);
     emptySeats.push(maxCapacity - currentRoundPeople); // Empty seats in the last round
   }
 
-  // Output the result
+  
   console.log("Groups in each round:", rounds);
   console.log("Empty seats in each round:", emptySeats);
   console.log("Total train rounds:", rounds.length);
   console.log("Reameanig of split group people :", leftBehind);
 }
 
-// Example usage
+
 let list = [3, 5, 7, 2, 3, 9, 1, 2, 9, 8, 3, 4, 1, 1, 9,10]; // Groups of people
 let maxCapacity = 8; // Maximum capacity per round
 calculateTrainRounds(list, maxCapacity);
